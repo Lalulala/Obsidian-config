a670cc77ec934f0302a3c9ac9e751e538625b88e
Xiong Ruhai <2452948962@qq.com>
2025-02-26 15:39:56 +0800

    sync local 2025-02-26

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ rmAndCp() {
 	rm -rf "$1"
 	cp -r "$2" "$3" 
 }
-
+git pull
 rmAndCp "../.obsidian" ".obsidian" "../"
 rmAndCp "../00 关于本资料库的使用方式" "./.00 关于本资料库的使用方式" "../00 关于本资料库的使用方式" 
 rmAndCp "../00-1 obsidian 模板" "./.00-1 obsidian 模板" "../00-1 obsidian 模板" 
